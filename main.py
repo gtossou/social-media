61327be90454364afd96d84baea40d3b5ae724ae
koffi <gil.tossou@gmail.com>
2025-01-24 21:43:32 +0100

    posting and retrieving routes

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+class UserPostIn(BaseModel):
+    body: str
+
+
+class UserPost(UserPostIn):
+    id: int
+
+
+post_table = {}
+
+
+@app.post("/", response_model=UserPost)
+async def create_post(post: UserPostIn):
+    data = post.model_dump()
+    last_record_id = len(post_table)
+    print(data)
+    new_post = {**data, "id": last_record_id}
+    post_table[last_record_id] = new_post
+    return new_post
+
+
+@app.get("/post/", response_model=list[UserPost])
+async def get_all_posts():
+    return list(post_table.values())
